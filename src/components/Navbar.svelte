a735b4e24963f9a23c5054f1e1263dfd026068b3
ajr250571 <ajr250571@gmail.com>
2024-05-26 16:20:09 -0300

    init repo

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,8 @@
+<div class="navbar bg-base-100 text-base-content border-b-2 my-2 justify-between items-center">
+	<button class="btn btn-ghost text-xl">MonoRepo</button>
+
+	<select class="select select-primary select-xs" data-choose-theme>
+		<option value="corporate">Light</option>
+		<option value="business">Dark</option>
+	</select>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	const handleClick = () => {
+		console.log('Boton click');
+	};
+</script>
+
+<h1 class="text-2xl text-primary">Bienvenido a SvelteKit</h1>
+<button on:click={handleClick} class="btn btn-primary mt-4">Boton</button>
+<div class="form-control">
+	<label class="label cursor-pointer">
+		<span class="label-text">Remember me</span>
+		<input type="checkbox" checked="checked" class="checkbox checkbox-primary" />
+	</label>
+</div>
+<input type="radio" name="radio-2" class="radio radio-primary" checked />
+<input type="radio" name="radio-2" class="radio radio-primary" />
