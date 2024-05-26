8da80603d343a1aca8e13d09ad67f5412a9840c9
ajr250571 <ajr250571@gmail.com>
2024-05-26 16:42:34 -0300

    condicional

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,23 @@
 <script>
+	let texto = '';
 	const handleClick = () => {
 		console.log('Boton click');
 	};
 </script>
 
-<h1 class="text-2xl text-primary">Bienvenido a SvelteKit</h1>
-<button on:click={handleClick} class="btn btn-primary mt-4">Boton</button>
-<div class="form-control">
-	<label class="label cursor-pointer">
-		<span class="label-text">Remember me</span>
-		<input type="checkbox" checked="checked" class="checkbox checkbox-primary" />
-	</label>
+<div class="flex flex-col w-96 justify-center">
+	<h1 class="text-2xl text-primary">Bienvenido a SvelteKit</h1>
+	<button on:click={handleClick} class="btn btn-primary mt-4">Boton</button>
+	<div class="form-control">
+		<label class="label cursor-pointer">
+			<span class="label-text">Remember me</span>
+			<input type="checkbox" checked="checked" class="checkbox checkbox-primary" />
+		</label>
+	</div>
+
+	<input type="text" bind:value={texto} class="input input-primary" />
+	<p>El texto tiene {texto.length} caracteres</p>
+	{#if texto.length > 0}
+		<button class="btn btn-secondary">Enviar Mensaje</button>
+	{/if}
 </div>
-<input type="radio" name="radio-2" class="radio radio-primary" checked />
-<input type="radio" name="radio-2" class="radio radio-primary" />
